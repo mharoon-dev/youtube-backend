900c1e92a25e947116019f78fdce9c7afc2dd5eb
mharoon-dev <haroon.aghani@gmail.com>
2024-05-30 14:46:23 +0500

    done

--- index.js
+++ index.js
@@ -6,24 +6,20 @@ import videoRouter from "./routes/videos.js";
 import authRouter from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import { upload } from "./controllers/video.js";
 
 const app = express();
-// app.use(upload.any());
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173" , "https://youtube-backend-one.vercel.app/", "https://youtube-nine-ruby.vercel.app/" ],
-//     credentials: true,
-//     allowedHeaders: ["Content-Type", "Authorization"], 
-//   })
-// );
-
-app.use(cors());
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173" ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
